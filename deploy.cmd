f9d6452cfa0d4053e7363c0174030f704b322ee7
ayrat(EKB) <gpsdom@gmai.com>
2013-06-21 20:53:30 +0400

    Еще кое чего подточил...

--- deploy.cmd
+++ deploy.cmd
@@ -2,5 +2,9 @@ cd %DEPLOY_PATH%
 call backup.bat ekb
 cd %BUILD_SERVER_PATH%
 echo Deploing...
-xcopy Ekb.Client.SL\ekb\*.* %DEPLOY_PATH%\ekb /EXCLUDE:deploy_exclude.lst /E /Y /R /I
-echo Done
+xcopy Ekb.Client.SL\ekb\*.* %DEPLOY_PATH%\ekb-tst /EXCLUDE:build-scripts\deploy_exclude.lst /E /Y /R /I
+if errorlevel 0 (
+echo Deploy success
+) else (
+echo Deploy failed
+)
